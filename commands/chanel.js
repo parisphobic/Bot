405c85d4ae99b708c0da0bf9e0fac3e0d0c608e3
parisphobic <yary235678@gmail.com>
2023-01-18 22:28:59 -0500

    1

--- commands/chanel.js
+++ commands/chanel.js
@@ -22,7 +22,7 @@ module.exports = {
 
     embed.setTitle('CHANEL THE BIGGIE')
     embed.setDescription('Chanel the big fat hippo does not know how to manage a roblox group and tries to SCAM hardworking people... Where my 9k robux at chanel?')
-    embed.setAuthor({ name: interaction.author.tag, iconURL: interaction.author.displayAvatarURL() })
+    embed.setAuthor({ name: interaction.author.name, iconURL: interaction.author.displayAvatarURL() })
     embed.setColor('2f3136')
     embed.setImage('https://cdn.discordapp.com/attachments/1039642210251051118/1064998621445095556/IMG_3186.png')
     interaction.reply({embeds: [embed]})
